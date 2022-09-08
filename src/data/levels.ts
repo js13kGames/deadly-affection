17f822093d0799c66fd21e337a975a65e0b7bff4
Martin Tale <me@martintale.com>
2022-09-08 17:09:27 +0100

    Finished levels

--- src/data/levels.ts
+++ src/data/levels.ts
@@ -24,178 +24,226 @@ export type Level = [LevelSize, Entity[]];
 const Empty = undefined;
 
 export const levels: Level[] = [
-	[
-		[3, 3],
-		[
-			Empty, [EntityEnd, Rotation0], Empty,
-			Empty, Empty, Empty,
-			Empty, [EntityStart, Rotation0, [Rotation270]], Empty,
-		],
-	],
-	[
-		[3, 3],
-		[
-			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation90],
-			Empty, Empty, Empty,
-			Empty, Empty, [EntityStart, Rotation0, [Rotation270]],
-		],
-	],
+	// 1 - done
 	[
 		[3, 5],
 		[
-			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation270],
+			[EntityTurnOneSide, Rotation0], Empty, [EntityTurnOneSide, Rotation180],
 			Empty, Empty, Empty,
-			[EntityCollectable, Rotation0], Empty, Empty,
+			[EntityCollectable, Rotation0], Empty, [EntityEnd, Rotation0],
 			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0]],
+			[EntityStart, Rotation0, [Rotation270]], Empty, Empty,
 		],
 	],
+	// 2 - done
 	[
-		[3, 5],
+		[3, 4],
 		[
-			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty,
-			[EntityEnd, Rotation0], Empty, [EntityStart, Rotation0, [Rotation0]],
-			[EntityCollectable, Rotation0], Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0, Rotation180]],
-		],
-	],
-	[
-		[3, 3],
-		[
-			Empty, [EntityEnd, Rotation0], Empty,
-			Empty, Empty, Empty,
-			Empty, [EntityStart, Rotation0, [Rotation0]], Empty,
-		],
-	],
-	[
-		[3, 3],
-		[
-			[EntityEnd, Rotation0], Empty, Empty,
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0]],
-		],
-	],
-	[
-		[3, 5],
-		[
-			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty,
-			[EntityCollectable, Rotation0], Empty, Empty,
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0]],
-		],
-	],
-	[
-		[3, 5],
-		[
-			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty,
+			[EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityEnd, Rotation0],
 			[EntityCollectable, Rotation0], Empty, [EntityCollectable, Rotation0],
-			[EntityCollectable, Rotation0], Empty, Empty,
-			[EntityTurnOneSide, Rotation90], [EntityCollectable, Rotation0], [EntityStart, Rotation0, [Rotation0]],
+			Empty, [EntityCollectable, Rotation0], Empty,
+			[EntityStart, Rotation0, [Rotation270]], [EntityTurnOneSide, Rotation90], [EntityTurnOneSide, Rotation90],
 		],
 	],
+	// 3 - done
 	[
-		[3, 3],
+		[4, 8],
 		[
-			Empty, [EntityEnd, Rotation0], Empty,
-			Empty, Empty, Empty,
-			Empty, [EntityStart, Rotation0, [Rotation0]], Empty,
+			Empty, Empty, [EntityEnd, Rotation0], Empty,
+			Empty, [EntityTurnOneSide, Rotation270], [EntitySplit, Rotation0], Empty,
+			[EntityTurnOneSide, Rotation270], Empty, [EntityTurnOneSide, Rotation270], Empty,
+			Empty, [EntityTurnOneSide, Rotation270], [EntityCollectable, Rotation0], [EntityTurnOneSide, Rotation270],
+			[EntityTurnOneSide, Rotation270], Empty, [EntityTurnOneSide, Rotation270], Empty,
+			Empty, [EntityTurnOneSide, Rotation270], [EntityCollectable, Rotation0], [EntityTurnOneSide, Rotation270],
+			[EntityTurnOneSide, Rotation270], Empty, [EntityTurnOneSide, Rotation270], Empty,
+			[EntityStart, Rotation0, [Rotation90]], [EntityEnd, Rotation0], Empty, Empty,
 		],
 	],
+	// 4 - done
 	[
-		[3, 3],
+		[5, 5],
 		[
-			[EntityEnd, Rotation0], Empty, Empty,
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0]],
+			Empty, Empty, [EntityEnd, Rotation0], Empty, Empty,
+			[EntityTurnOneSide, Rotation0], [EntitySplit, Rotation0], [EntitySplit, Rotation180], [EntitySplit, Rotation0], [EntityTurnOneSide, Rotation270],
+			[EntityCollectable, Rotation0], Empty, Empty, Empty, [EntityCollectable, Rotation0],
+			Empty, [EntityTurnOneSide, Rotation0], [EntitySplit, Rotation0], [EntityTurnOneSide, Rotation270], Empty,
+			[EntityTurnOneSide, Rotation0], [EntityTurnOneSide, Rotation0], [EntityStart, Rotation0, [Rotation0]], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
 		],
 	],
+	// 5 - done
 	[
-		[3, 5],
+		[5, 5],
 		[
-			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty,
-			[EntityCollectable, Rotation0], Empty, Empty,
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0]],
+			[EntityTurnOneSide, Rotation0], Empty, [EntitySplit, Rotation270], Empty, [EntityTurnOneSide, Rotation270],
+			Empty, Empty, [EntityCollectable, Rotation0], Empty, Empty,
+			[EntitySplit, Rotation270], [EntityCollectable, Rotation0], [EntityEnd, Rotation0], [EntityCollectable, Rotation0], [EntitySplit, Rotation270],
+			Empty, Empty, [EntityTurnOneSide, Rotation180], [EntityEnd, Rotation0], Empty,
+			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation90, Rotation180]], Empty, [EntityEnd, Rotation0],
 		],
 	],
+	// 6 - done
 	[
-		[3, 5],
+		[5, 5],
 		[
-			[EntityEnd, Rotation0], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation270], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityCollectable, Rotation0], Empty, [EntityTunnel, Rotation0],
-			[EntityTurnOneSide, Rotation90], [EntityBlock, Rotation0], [EntityStart, Rotation0, [Rotation0]],
+			[EntityTurnOneSide, Rotation90], Empty, [EntityEnd, Rotation0], [EntityEnd, Rotation0], [EntityTurnOneSide, Rotation270],
+			Empty, [EntityEnd, Rotation0], [EntityBlock, Rotation0], [EntityBlock, Rotation0], Empty,
+			Empty, [EntitySplit, Rotation0], [EntityCollectable, Rotation0], [EntityCollectable, Rotation0], [EntitySplit, Rotation0],
+			Empty, [EntityEnd, Rotation0], [EntityStart, Rotation0, [Rotation0]], [EntityStart, Rotation0, [Rotation0]], Empty,
+			[EntityTurnOneSide, Rotation90], Empty, [EntityTurnOneSide, Rotation90], [EntityTurnOneSide, Rotation90], [EntityTurnOneSide, Rotation90],
 		],
 	],
+	// 7 - done
 	[
-		[5, 10],
+		[5, 7],
 		[
-			[EntityEnd, Rotation0], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty, [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation270], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityCollectable, Rotation0], Empty, [EntityTunnel, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation90], [EntityBlock, Rotation0], [EntityBlock, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty, [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation270], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityCollectable, Rotation0], Empty, [EntityTunnel, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation90], [EntityBlock, Rotation0], [EntityStart, Rotation0, [Rotation0]], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
+			[EntityTurnOneSide, Rotation270], [EntitySplit, Rotation0], [EntityTurnOneSide, Rotation0], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
+			Empty, [EntityCollectable, Rotation0], [EntityEnd, Rotation0], [EntityCollectable, Rotation0], Empty,
+			Empty, [EntityEnd, Rotation0], [EntityBlock, Rotation0], [EntityEnd, Rotation0], Empty,
+			Empty, Empty, [EntityCollectable, Rotation0], Empty, [EntitySplit, Rotation90],
+			Empty, Empty, [EntityBlock, Rotation0], Empty, Empty,
+			Empty, [EntityBlock, Rotation0], [EntityStart, Rotation0, [Rotation90]], [EntityBlock, Rotation0], Empty,
+			[EntityTurnOneSide, Rotation90], Empty, [EntitySplit, Rotation270], Empty, [EntityTurnOneSide, Rotation180],
 		],
 	],
+	// 8 - done
 	[
-		[3, 3],
+		[5, 7],
 		[
-			Empty, [EntityEnd, Rotation0], Empty,
-			Empty, Empty, Empty,
-			Empty, [EntityStart, Rotation0, [Rotation0]], Empty,
+			Empty, Empty, [EntityEnd, Rotation0], Empty, Empty,
+			Empty, Empty, Empty, Empty, Empty,
+			[EntityTurnOneSide, Rotation180], Empty, [EntityTurnBothSides, Rotation90], Empty, [EntityTurnOneSide, Rotation90],
+			Empty, Empty, [EntityCollectable, Rotation0], Empty, Empty,
+			[EntitySplit, Rotation0], Empty, [EntityTurnBothSides, Rotation0], Empty, [EntitySplit, Rotation0],
+			[EntityCollectable, Rotation0], Empty, Empty, Empty, [EntityCollectable, Rotation0],
+			[EntityEnd, Rotation0], Empty, [EntityStart, Rotation0, [Rotation270]], Empty, [EntityEnd, Rotation0],
 		],
 	],
+	// 9 - done
 	[
-		[3, 3],
+		[5, 6],
 		[
-			[EntityEnd, Rotation0], Empty, Empty,
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0]],
+			Empty, [EntityTurnOneSide, Rotation270], [EntityCollectable, Rotation0], [EntityEnd, Rotation0], Empty,
+			[EntityTurnOneSide, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation270], [EntityTurnBothSides, Rotation90], [EntityTurnOneSide, Rotation270],
+			[EntityCollectable, Rotation0], Empty, Empty, Empty, [EntityCollectable, Rotation0],
+			[EntityTurnOneSide, Rotation270], [EntityTurnBothSides, Rotation90], [EntitySplit, Rotation0], [EntityTurnBothSides, Rotation0], [EntityTurnOneSide, Rotation90],
+			Empty, [EntityTurnOneSide, Rotation180], Empty, [EntityTurnOneSide, Rotation180], Empty,
+			Empty, Empty, [EntityStart, Rotation0, [Rotation0]], Empty, Empty,
 		],
 	],
+	// 10 - done
 	[
-		[3, 5],
+		[5, 5],
 		[
-			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty,
-			[EntityCollectable, Rotation0], Empty, Empty,
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation90], Empty, [EntityStart, Rotation0, [Rotation0]],
+			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation90], [EntitySplit, Rotation0], [EntityEnd, Rotation0],
+			Empty, Empty, Empty, [EntityEnd, Rotation0], Empty,
+			[EntitySplit, Rotation180], Empty, [EntityTurnBothSides, Rotation0], Empty, [EntityStart, Rotation0, [Rotation0, Rotation270]],
+			[EntityCollectable, Rotation0], [EntityEnd, Rotation0], [EntityCollectable, Rotation0], Empty, [EntityCollectable, Rotation0],
+			[EntityTurnOneSide, Rotation0], [EntitySplit, Rotation270], [EntityTurnOneSide, Rotation180], Empty, [EntityEnd, Rotation0],
 		],
 	],
+	// 11 - done
 	[
-		[3, 5],
+		[6, 8],
 		[
-			[EntityEnd, Rotation0], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty,
-			[EntityTurnOneSide, Rotation270], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityCollectable, Rotation0], Empty, [EntityTunnel, Rotation0],
-			[EntityTurnOneSide, Rotation90], [EntityBlock, Rotation0], [EntityStart, Rotation0, [Rotation0]],
+			Empty, Empty, [EntityEnd, Rotation0], [EntityCollectable, Rotation0], Empty, [EntityTurnBothSides, Rotation0],
+			Empty, [EntityTurnBothSides, Rotation0], [EntityCollectable, Rotation0], [EntityTurnBothSides, Rotation0], Empty, Empty,
+			[EntityEnd, Rotation0], Empty, [EntityTunnel, Rotation0], Empty, [EntityStart, Rotation0, [Rotation0]], Empty,
+			[EntityStart, Rotation0, [Rotation90]], Empty, [EntityTunnel, Rotation0], Empty, [EntityEnd, Rotation0], Empty,
+			[EntityEnd, Rotation0], Empty, [EntityTunnel, Rotation0], Empty, [EntityStart, Rotation0, [Rotation180]], Empty,
+			Empty, Empty, Empty, Empty, Empty, Empty,
+			[EntityTurnBothSides, Rotation0], [EntityTurnBothSides, Rotation0], [EntityStart, Rotation0, [Rotation0]], [EntityTurnBothSides, Rotation0], Empty, Empty,
+			[EntityTurnBothSides, Rotation0], Empty, [EntityCollectable, Rotation0], Empty, Empty, [EntityTurnBothSides, Rotation0],
 		],
 	],
+	// 12 - done
 	[
-		[5, 10],
+		[5, 6],
 		[
-			[EntityEnd, Rotation0], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty, [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation270], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityCollectable, Rotation0], Empty, [EntityTunnel, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation90], [EntityBlock, Rotation0], [EntityBlock, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270], [EntityTurnOneSide, Rotation270],
-			Empty, Empty, Empty, [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation270], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityCollectable, Rotation0], Empty, [EntityTunnel, Rotation0], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
-			[EntityTurnOneSide, Rotation90], [EntityBlock, Rotation0], [EntityStart, Rotation0, [Rotation0]], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0],
+			[EntityEnd, Rotation0], Empty, [EntityTurnOneSide, Rotation270], Empty, Empty,
+			Empty, Empty, Empty, Empty, Empty,
+			[EntityTurnOneSide, Rotation270], Empty, [EntityTurnBothSides, Rotation0], Empty, [EntityTurnOneSide, Rotation90],
+			[EntityCollectable, Rotation0], Empty, [EntityCollectable, Rotation0], Empty, [EntityStart, Rotation0, [Rotation90, Rotation270]],
+			[EntityTurnOneSide, Rotation90], Empty, [EntityTurnBothSides, Rotation0], [EntityStart, Rotation0, [Rotation0]], [EntityCollectable, Rotation0],
+			Empty, Empty, [EntityEnd, Rotation0], Empty, [EntityEnd, Rotation0],
+		],
+	],
+	// 13 - done
+	[
+		[5, 6],
+		[
+			Empty, Empty, [EntityEnd, Rotation0], Empty, Empty,
+			Empty, [EntityTurnBothSides, Rotation90], [EntityTurnBothSides, Rotation90], Empty, [EntityTurnBothSides, Rotation90],
+			Empty, [EntityCollectable, Rotation0], [EntityCollectable, Rotation0], Empty, Empty,
+			Empty, [EntityCollectable, Rotation0], [EntityTurnBothSides, Rotation90], Empty, [EntitySplit, Rotation0],
+			[EntityEnd, Rotation0], [EntityTurnBothSides, Rotation0], Empty, [EntityStart, Rotation0, [Rotation180]], Empty,
+			[EntityEnd, Rotation0], [EntitySplit, Rotation270], Empty, Empty, [EntityTurnBothSides, Rotation0],
+		],
+	],
+	// 14 - done
+	[
+		[6, 8],
+		[
+			Empty, [EntityTurnOneSide, Rotation180], Empty, Empty, [EntitySplit, Rotation180], [EntityTurnOneSide, Rotation0],
+			Empty, [EntityTurnBothSides, Rotation90], [EntitySplit, Rotation180], [EntityTurnBothSides, Rotation90], Empty, Empty,
+			Empty, Empty, [EntityTunnel, Rotation0], [EntityCollectable, Rotation0], Empty, Empty,
+			Empty, [EntityTunnel, Rotation90], [EntityStart, Rotation0, [Rotation0, Rotation90, Rotation180, Rotation270]], [EntityTunnel, Rotation90], [EntityEnd, Rotation0], [EntityEnd, Rotation0],
+			[EntityEnd, Rotation0], [EntityCollectable, Rotation0], [EntityTunnel, Rotation0], [EntitySplit, Rotation0], Empty, Empty,
+			Empty, [EntityEnd, Rotation0], Empty, [EntitySplit, Rotation270], Empty, Empty,
+			Empty, [EntityEnd, Rotation0], Empty, [EntitySplit, Rotation90], Empty, Empty,
+			Empty, Empty, Empty, [EntityCollectable, Rotation0], Empty, Empty,
+		],
+	],
+	// 15 - done
+	[
+		[5, 8],
+		[
+			[EntityTurnOneSide, Rotation270], Empty, [EntityCollectable, Rotation0], Empty, [EntityTurnOneSide, Rotation270],
+			Empty, [EntityEnd, Rotation0], Empty, Empty, Empty,
+			Empty, [EntitySplit, Rotation0], [EntityTurnOneSide, Rotation180], Empty, [EntityCollectable, Rotation0],
+			[EntityStart, Rotation0, [Rotation90]], [EntityTunnel, Rotation90], [EntityTunnel, Rotation90], Empty, [EntityEnd, Rotation0],
+			[EntityStart, Rotation0, [Rotation90]], [EntityTunnel, Rotation90], [EntityTunnel, Rotation90], [EntityEnd, Rotation0], Empty,
+			[EntityStart, Rotation0, [Rotation90]], [EntityTunnel, Rotation90], [EntityTunnel, Rotation90], Empty, [EntityEnd, Rotation0],
+			Empty, [EntityStart, Rotation0, [Rotation90]], [EntityEnd, Rotation0], Empty, Empty,
+			[EntityTurnOneSide, Rotation270], [EntityCollectable, Rotation0], Empty, [EntitySplit, Rotation180], [EntityTurnOneSide, Rotation270],
+		],
+	],
+	// 16
+	[
+		[6, 6],
+		[
+			Empty, [EntityEnd, Rotation0], Empty, Empty, Empty, Empty,
+			[EntityEnd, Rotation0], [EntityTurnBothSides, Rotation90], [EntityTurnBothSides, Rotation90], Empty, Empty, Empty,
+			Empty, [EntityTurnBothSides, Rotation90], [EntityStart, Rotation0, [Rotation0, Rotation90, Rotation180, Rotation270]], [EntityTurnBothSides, Rotation90], Empty, Empty,
+			Empty, Empty, [EntityTurnBothSides, Rotation90], [EntityTurnBothSides, Rotation90], [EntityCollectable, Rotation0], [EntityEnd, Rotation0],
+			Empty, Empty, Empty, [EntityCollectable, Rotation0], Empty, Empty,
+			Empty, Empty, Empty, [EntityEnd, Rotation0], Empty, Empty,
+		],
+	],
+	// 17 - done
+	[
+		[6, 7],
+		[
+			[EntityEnd, Rotation0], [EntityTurnOneSide, Rotation90], Empty, [EntityCollectable, Rotation0], Empty, [EntityEnd, Rotation0],
+			Empty, Empty, [EntityTurnOneSide, Rotation270], Empty, Empty, [EntityTurnOneSide, Rotation180],
+			Empty, [EntityCollectable, Rotation0], Empty, [EntityTurnOneSide, Rotation0], [EntityTurnOneSide, Rotation270], [EntityCollectable, Rotation0],
+			[EntitySplit, Rotation0], Empty, Empty, [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation270], Empty, 
+			[EntitySplit, Rotation90], Empty, [EntityTurnBothSides, Rotation0], Empty, Empty, [EntitySplit, Rotation0],
+			[EntityStart, Rotation0, [Rotation180, Rotation270]], [EntityTurnBothSides, Rotation0], [EntitySplit, Rotation0], Empty, Empty, Empty,
+			Empty, [EntityEnd, Rotation0], [EntityEnd, Rotation0], [EntityEnd, Rotation0], [EntityEnd, Rotation0], [EntityEnd, Rotation0],
+		],
+	],
+	// 18 - done
+	[
+		[6, 8],
+		[
+			Empty, [EntityEnd, Rotation0], Empty, [EntityEnd, Rotation0], Empty, [EntityEnd, Rotation0],
+			[EntityTurnOneSide, Rotation180], Empty, Empty, [EntityCollectable, Rotation0], Empty, [EntitySplit, Rotation180],
+			[EntityTurnOneSide, Rotation90], [EntitySplit, Rotation90], Empty, [EntityTunnel, Rotation90], Empty, [EntitySplit, Rotation180],
+			Empty, Empty, [EntityTurnBothSides, Rotation0], [EntityCollectable, Rotation0], [EntityTurnBothSides, Rotation0], Empty,
+			Empty, [EntityTunnel, Rotation0], Empty, Empty, Empty, [EntityTunnel, Rotation0],
+			Empty, Empty, Empty, [EntityCollectable, Rotation0], [EntityStart, Rotation0, [Rotation90]], Empty,
+			Empty, [EntityStart, Rotation0, [Rotation0]], [EntityEnd, Rotation0], [EntityTunnel, Rotation90], Empty, [EntitySplit, Rotation270],
+			Empty, Empty, Empty, [EntityStart, Rotation0, [Rotation0]], Empty, [EntityEnd, Rotation0],
 		],
 	],
 ];
